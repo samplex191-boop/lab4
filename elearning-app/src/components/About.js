0b4c7cd4b6808976c1f09a2f6f3442cc406e7c27
samplex191-boop <samplex191@gmail.com>
2026-03-02 05:36:34 +0000

    lab4 done

---
+++ elearning-app/src/components/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function About() {
+  return (
+    <div style={{
+      maxWidth: "700px",
+      margin: "40px auto",
+      lineHeight: "1.6",
+      backgroundColor: "#f8fafc",
+      padding: "30px",
+      borderRadius: "10px"
+    }}>
+      <h1>About Us</h1>
+      <p>
+        We provide high-quality online courses designed to help students
+        master modern web technologies.
+      </p>
+    </div>
+  );
+}
+
+export default About;
---
+++ elearning-app/src/components/Contact.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div style={{
+      maxWidth: "600px",
+      margin: "40px auto",
+      backgroundColor: "#f8fafc",
+      padding: "30px",
+      borderRadius: "10px"
+    }}>
+      <h1>Contact Us</h1>
+
+      <p>Email: support@elearning.com</p>
+      <p>Phone: +123 456 7890</p>
+    </div>
+  );
+}
+
+export default Contact;
